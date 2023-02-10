55ef3fc477573fd188db39dbb4a0c8ba7557f643
Vedvyas <Vedvyas.R@coforge.com>
2023-02-10 11:31:26 +0530

    Test ved

---
+++ TestVed/Add.py
@@ -0,0 +1,23 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2639
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red254\green213\blue177;\red42\green45\blue49;\red201\green201\blue201;
+\red197\green136\blue83;\red184\green93\blue213;\red136\green185\blue102;\red203\green203\blue203;}
+{\*\expandedcolortbl;;\cssrgb\c100000\c86667\c74510;\cssrgb\c21961\c23137\c25098;\cssrgb\c82745\c82745\c82745;
+\cssrgb\c81961\c60392\c40000;\cssrgb\c77647\c47059\c86667;\cssrgb\c59608\c76471\c47451;\cssrgb\c83529\c83529\c83529;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 # This program adds two numbers\cf4 \strokec4 \
+\
+num1 = \cf5 \strokec5 1.5\cf4 \strokec4 \
+num2 = \cf5 \strokec5 6.3\cf4 \strokec4 \
+\
+\cf2 \strokec2 # Add two numbers\cf4 \strokec4 \
+sum = num1 + num2\
+\
+\cf2 \strokec2 # Display the sum\cf4 \strokec4 \
+\pard\pardeftab720\partightenfactor0
+\cf6 \strokec6 print\cf4 \strokec4 (\cf7 \strokec7 'The sum of \{0\} and \{1\} is \{2\}'\cf4 \strokec4 .format(num1, num2, sum))\cf8 \strokec8 \
+}
